6fa7e396de4cc0b3ecf190bb029d2ad1837c3788
Diego Delevedove de Oliveira <diego-delevedove.oliveira@serpro.gov.br>
2025-04-14 18:03:20 -0300

    ajustando 3

--- server/db.js
+++ server/db.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/conecta_mongo')
+//Se quiser usar o atlas(mongo online),instalar dependencia abaixo
+//npm install mongoose dotenv
+//Usar a seguinte conexão
+//MONGO_URI=mongodb+srv://<username>:<password>@cluster0.mongodb.net/mydatabase?retryWrites=true&w=majority 
 
 const db = mongoose.connection;
 
